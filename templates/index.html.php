7582e35a4798557070fefea865d54b76fa242e38
Giulia Pierbon <giulia.pierbon@sauro.net>
2020-03-24 17:30:01 +0100

    index file clean up

---
+++ templates/index.html.php
@@ -0,0 +1,17 @@
+<?php include_once __DIR__.'/../include/header.php';?>
+
+<main style="margin-top: 100px;" >
+    <div class="row alert-light m-xl-5" style="color: #6bafba; text-align:left; ">
+        <div class="col-xl-12" style="color: #6bafba; margin-top: 20px;">
+            <h1> <?=$title?></h1>
+        </div>
+        <hr style=" border-top: 1px solid #6bafba;">
+        <div style="margin-top: 20px; margin-left: 20px;">
+            <h3>  <?=$title_home?></h3>
+            <h3>  <?=$message_home?></h3>
+        </div>
+    </div>
+
+</main>
+
+<?php include_once __DIR__.'/../include/footer.php';?>
